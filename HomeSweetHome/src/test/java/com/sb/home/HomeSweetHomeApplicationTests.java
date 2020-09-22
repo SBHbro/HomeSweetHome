edc408cdfaeb2b773752a5e1a2efd03bd356efa3
SBHbro <ssd018@gmail.com>
2020-09-22 21:32:34 +0900

    Add BE Project

---
+++ HomeSweetHome/src/test/java/com/sb/home/HomeSweetHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSweetHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
